fb4fe1e692ebbc281e9aed00f67bc15e17cd96cc
Gustavo <38637402+characteress@users.noreply.github.com>
2018-07-06 21:15:26 -0300

    Update screwd_z2_plus.mk

--- products/screwd_z2_plus.mk
+++ products/screwd_z2_plus.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-PRODUCT_NAME := z2_plus
+PRODUCT_NAME := screwd_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
